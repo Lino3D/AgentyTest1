c30fb9b1ad7cc4d782dfeb5a4cedd047494c8167
Lino3D <kamiskijakub@gmail.com>
2017-04-18 21:00:00 +0200

    Przesyłanie wiadomości w sieci

--- src/Manager.java
+++ src/Manager.java
@@ -2,12 +2,14 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SenderBehaviour;
 import jade.lang.acl.ACLMessage;
 
 /**
  * Created by Jakub on 09.03.2017.
  */
-public class Manager extends Agent {
+public class Manager extends Agent
+{
     private boolean isFree = false;
     //Override
     protected void setup()
@@ -30,12 +32,11 @@ public class Manager extends Agent {
                     System.out.println(response.getContent());
                 }
             }
-
         };
                 addBehaviour(mainBehaviour);
-                }
-private ACLMessage createMessage()
-        {
+    }
+    private ACLMessage createMessage()
+    {
         ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
         AID receiverJames = new AID( "James@192.168.1.112:1099/JADE", AID.ISGUID);
         receiverJames.addAddresses("http://192.168.1.112:7778/acc");
@@ -46,5 +47,5 @@ private ACLMessage createMessage()
         msg.setOntology("Weather-Forecast");
         msg.setContent("Today is raining");
         return msg;
-        }
-        }
+    }
+}
