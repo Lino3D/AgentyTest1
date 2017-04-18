16b176a225baaa5e50cba0e6fcca107f052a9e42
grabkipower <michcio91@gmail.com>
2017-04-18 21:39:59 +0200

    Init commit michal

---
+++ src/Address.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Mike on 18.04.2017.
+ */
+public class Address {
+    public String ComputerAdress;
+    public String AgentAdress;
+}
---
+++ src/EMAAgent.java
@@ -0,0 +1,14 @@
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SenderBehaviour;
+import jade.lang.acl.ACLMessage;
+import java.util.List;
+
+/**
+ * Created by Mike on 18.04.2017.
+ */
+public class EMAAgent  extends Agent{
+    List<Address> Addresses;
+}
