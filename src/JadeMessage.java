335ef0a8ec23c7f6035692883f0c93f883d9ecb5
Michał Grabowski <michal.grabowski@wum.edu.pl>
2017-04-22 13:13:50 +0200

    Michal commit changes - zaraz bedzie bol przy mergowaniu

--- src/JadeMessage.java
+++ src/JadeMessage.java
@@ -2,14 +2,14 @@
  * Created by michał.grabowski on 22.04.2017.
  */
 public class JadeMessage {
-public JadeMessage(int id, String[] message) {
+    public JadeMessage(int id, String message) {
         Id = id;
         Message = message;
         IsSent = false;
-        }
-
-public int Id;
-public String Message[];
-public Boolean IsSent;
-public Boolean WasRespondedTo;
-        }
+    }
+    public int Id;
+    public String Message;
+    public Boolean IsSent;
+    public Boolean WasRespondedTo;
+    public Boolean WasReceived;
+}
